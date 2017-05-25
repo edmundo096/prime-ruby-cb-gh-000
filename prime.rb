d9c2a4eb6376a8a25075e8e697239e21cd911a5b
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-05-25 00:15:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+# Based from https://stackoverflow.com/questions/3594345/ruby-determine-if-a-number-is-a-prime
+def prime?(integer)
+  # Negatives, 0 and 1 returns false.
+  return false if integer < 2
+
+  # Check for divisable numbers below the specified integer. If divisable then isn't prime. (Not efficient!)
+  for n_check in 2..(integer - 1)
+    if (integer % n_check) == 0
+      return false
+    end
+  end
+
+  true
+end
